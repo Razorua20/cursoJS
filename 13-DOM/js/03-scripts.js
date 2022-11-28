75d03ae51f07410f2eb03a530a6502beb114148f
Razorua20 <simon.delafuente20@gmail.com>
2022-11-28 17:31:12 -0300

    Avance hasta capitulo 22

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,11 @@
+//Elementos por id
+
+const formulario = document.getElementById('formulario');
+
+console.log(formulario);
+
+const nohay = document.getElementById('nohay');
+
+console.log(nohay);
+
+//Recuerda que no se debe poner 2 id iguales en 1 documento
